b40294b34578c6a9b50c3e987108819c66cb8398
seragelgamal <55516965+seragelgamal@users.noreply.github.com>
2020-07-06 13:34:27 -0600

    commit

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ cnv.width = 1000;
 cnv.height = 1000;
 
 // draw targets
-function drawTarget(x, y, r, ringNumber, colour) {
-    ctx.strokeStyle = colour;
+function drawTarget(x, y, r, ringNumber, ringColour) {
+    ctx.strokeStyle = ringColour;
     let gap = r / ringNumber;
     let ringRadius = r;
     for (let n = 0; n < ringNumber; n++) {
@@ -15,9 +15,12 @@ function drawTarget(x, y, r, ringNumber, colour) {
     }
 }
 
+drawTarget(100, 100, 75, 4, 'blue');
+drawTarget(100, 250, 46, 10, 'purple');
+
 // draw snowmen
-function drawSnowman(bottomSnowBallX, bottomSnowBallY, bottomSnowBallR, snowballNumber, colour) {
-    ctx.strokeStyle = colour;
+function drawSnowman(bottomSnowBallX, bottomSnowBallY, bottomSnowBallR, snowballNumber, lineColour) {
+    ctx.strokeStyle = lineColour;
     let y = bottomSnowBallY;
     let r = bottomSnowBallR;
     for (let n = 0; n < snowballNumber; n++) {
@@ -28,13 +31,22 @@ function drawSnowman(bottomSnowBallX, bottomSnowBallY, bottomSnowBallR, snowball
     }
 }
 
+drawSnowman(110, 670, 100, 3, 'red');
+drawSnowman(270, 500, 75, 5, 'orange');
+
+
 // draw grids
-function drawGrid(topLeftX, topLeftY, boxW, boxH, columnNumber, rowNumber, colour) {
-    ctx.strokeStyle = colour;
+function drawGrid(topLeftX, topLeftY, boxW, boxH, columnNumber, rowNumber, lineColour) {
+    ctx.strokeStyle = lineColour;
+    // rows
     for (let n = 0; n < rowNumber + 1; n++) {
-        line(topLeftX, topLeftY + boxH*n, topLeftX + boxW*rowNumber, topLeftY + boxH*n);
+        line(topLeftX, topLeftY + boxH * n, topLeftX + boxW * columnNumber, topLeftY + boxH * n);
     }
+    // columns
     for (let n = 0; n < columnNumber + 1; n++) {
-        line(topLeftX + boxW*n, topLeftY, topLeftX + boxH*n, topLeftY + boxH*columnNumber);
+        line(topLeftX + boxW * n, topLeftY, topLeftX + boxW * n, topLeftY + boxH * rowNumber);
     }
 }
+
+drawGrid(350, 100, 50, 30, 10, 4, 'green');
+drawGrid(375, 250, 45, 100, 3, 6, 'yellow');
